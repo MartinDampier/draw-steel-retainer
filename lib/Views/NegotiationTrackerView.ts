e5983a61bc6ed1bea3e92e81668340be820ec924
MartinSDampier <martinsdampier@gmail.com>
2025-10-13 13:29:54 -0500

    Womp womp 23

--- lib/Views/NegotiationTrackerView.ts
+++ lib/Views/NegotiationTrackerView.ts
@@ -1,7 +1,8 @@
 import Creature from 'lib/Models/Creature';
 import { Red, Green, Orange, Yes, No, Fill, NEGOTIATION_VIEW } from 'lib/Models/Constants';
-import { ButtonComponent, ItemView, TextAreaComponent, WorkspaceLeaf, Setting, TextComponent, ExtraButtonComponent, DropdownComponent, Modal, App } from 'obsidian';
+import { ButtonComponent, ItemView, TextAreaComponent, WorkspaceLeaf, Setting, TextComponent, ExtraButtonComponent, DropdownComponent, Modal, App, sanitizeHTMLToDom, SliderComponent } from 'obsidian';
 import { CreatureTypes } from 'lib/Models/CreatureTypes';
+import * as fs from 'fs';
 
 export class NegotiationView extends ItemView {
 
@@ -19,5 +20,12 @@ export class NegotiationView extends ItemView {
     constructor(leaf: WorkspaceLeaf, app: App) {
         super(leaf);
         this.app = app;
+    }
+
+    async onOpen() {
+        this.contentEl.empty();
+        let mainDiv = this.contentEl.createDiv({cls: ""})
+        let patienceDiv = mainDiv.createDiv();
+        
     }
 }
